c41cc439eed5660a4265508520cd298bcc169c93
dani <jdmessons@esliceu.net>
2026-01-16 15:54:56 +0100

    functions.php

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function infobasic_config(){
+
+    echo "EJECUTAMOS CONFIGURACION";
+
+}
+add_action( 'after_setup_theme', 'infobasic_config', 0);
+
+function infobasic_scripts(){
+    wp_enqueue_script( "bootstrap_js", get_theme_file_uri("inc/js/bootstrap.min.js"), array("jquery"),"4.5", true );
+    wp_enqueue_style( "bootstrap_css", get_theme_file_uri("inc/css/bootstrap.min.css"),array(), "4.5","all" );
+
+}
+add_action( 'wp_enqueue_scripts', 'infobasic_scripts');
--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <?php wp_head();?>
     <title>Bienvenidos a mi web</title>
 </head>
