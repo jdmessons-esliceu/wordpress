da3c1310ed15e83669797179f11729297d07600e
dani <jdmessons@esliceu.net>
2026-01-22 15:52:34 +0100

    site url

--- header.php
+++ header.php
@@ -10,20 +10,20 @@
 
     <div class="cabecera">
     <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <a class="navbar-brand" href="/">Home</a>
+        <a class="navbar-brand" href="<?php echo site_url( "/") ?>">Home</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="blog">Blog <span class="sr-only"></span></a>
+                <a class="nav-link" href="<?php echo site_url( "blog") ?>">Blog <span class="sr-only"></span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="acerca-de-nosotros">Acerca de nosotros</a>
+                <a class="nav-link" href="<?php echo site_url( "acerca-de-nosotros") ?>">Acerca de nosotros</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="contacto">Contacto</a>
+                <a class="nav-link" href="<?php echo site_url( "contacto") ?>">Contacto</a>
             </li>
             </ul>
         </div>
