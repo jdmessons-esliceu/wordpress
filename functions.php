b3ec3265d18ae63e4287f223e5e8eefe15079a50
dani <jdmessons@esliceu.net>
2026-01-22 16:04:20 +0100

    mover todo a inc

--- functions.php
+++ functions.php
@@ -12,8 +12,8 @@ function infobasic_config(){
 add_action( 'after_setup_theme', 'infobasic_config', 0);
 
 function infobasic_scripts(){
-    wp_enqueue_script( "bootstrap_js", get_theme_file_uri("/js/bootstrap.min.js"), array("jquery"),"4.5", true );
-    wp_enqueue_style( "bootstrap_css", get_theme_file_uri("/css/bootstrap.min.css"),array(), "4.5","all" );
+    wp_enqueue_script( "bootstrap_js", get_theme_file_uri("/inc/js/bootstrap.min.js"), array("jquery"),"4.5", true );
+    wp_enqueue_style( "bootstrap_css", get_theme_file_uri("/inc/css/bootstrap.min.css"),array(), "4.5","all" );
 
 }
 add_action( 'wp_enqueue_scripts', 'infobasic_scripts');
