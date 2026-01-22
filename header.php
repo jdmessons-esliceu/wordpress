1f9be11623d725b00c4bd6f1119009976a75af86
dani <jdmessons@esliceu.net>
2026-01-22 15:41:47 +0100

    Problema de header solucionado (me daba error, solo podia estar en home. Luego no se le aplicaba el estilo correctamente)

--- header.php
+++ header.php
@@ -8,4 +8,24 @@
 </head>
 <body <?php body_class();?>>
 
-    <h1>Menú</h1>
+    <div class="cabecera">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <a class="navbar-brand" href="/">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="blog">Blog <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="acerca-de-nosotros">Acerca de nosotros</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contacto">Contacto</a>
+            </li>
+            </ul>
+        </div>
+        </nav>
+    </div>
